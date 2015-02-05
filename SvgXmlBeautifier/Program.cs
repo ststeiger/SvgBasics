473d3f4753028b1da2db1310e37ca21e23c9340e
ststeiger <quandary82@hailmail.net>
2015-02-05 10:15:56 +0100

    Merging

--- SvgXmlBeautifier/Program.cs
+++ SvgXmlBeautifier/Program.cs
@@ -1,20 +1,38 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
+
 namespace SvgXmlBeautifier
 {
+
+
     static class Program
     {
+
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
-        }
+            bool bShowWindow = false;
+            if (bShowWindow)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmMain());
+            }
+
+            Beautifier.CleanSVG();
+            System.Console.WriteLine(" --- Finished ! --- ");
+            System.Threading.Thread.Sleep(1000);
+        } // End Sub Main
+
+
     }
+
+
 }
