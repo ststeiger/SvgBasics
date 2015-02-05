2f1d4764326284d3139dbde2c15cc5dd7ced5fbc
ststeiger <quandary82@hailmail.net>
2015-02-05 11:51:26 +0100

    Changed Winforms to Console

--- SvgXmlBeautifier/Properties/AssemblyInfo.cs
+++ SvgXmlBeautifier/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2015.2.5.1141")]
-[assembly: AssemblyFileVersion("2015.2.5.1141")]
+[assembly: AssemblyVersion("2015.2.5.1151")]
+[assembly: AssemblyFileVersion("2015.2.5.1151")]
